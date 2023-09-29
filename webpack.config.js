e6d01e3d0f4b75fd7f1ab5ba8b6aa7fb343e0f6f
munkhmngl <munkhmngl@gmail.com>
2023-09-29 16:27:06 +0800

    webpack dev server ajilagaand orloo

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,19 @@ const path = require('path');
 const postCSSPlugins = [require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer'), require('postcss-import')];
 module.exports = {
     mode: 'development',
-    watch: true,
     entry: './app/scripts/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        // before: function(app, server){
+        //     server._watch("./app/**/*.html")
+        // },
+        static: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
     },
     module: {
         rules: [
