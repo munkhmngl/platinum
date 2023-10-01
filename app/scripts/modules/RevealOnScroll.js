9e09afe329d6a06e4959a4eb832bc1d5c7d42bab
munkhmngl <munkhmngl@gmail.com>
2023-10-01 16:39:12 +0800

    scroll complate full

--- app/scripts/modules/RevealOnScroll.js
+++ app/scripts/modules/RevealOnScroll.js
@@ -1,14 +1,21 @@
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 export default class RevealOnScroll {
-    constructor(){
-        this.itemsToReveal = document.querySelectorAll('.feature-item');
+    constructor(els, revealPoint){
+        this.revealPoint = revealPoint;
+        this.itemsToReveal = els;
+        this.browserHeight = window.innerHeight;
         this.hideInitially();
         this.scrollThroltte = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events(){
-        window.addEventListener('scroll', this.scrollThroltte)
+        window.addEventListener('scroll', this.scrollThroltte);
+        window.addEventListener('resize', debounce(()=>{
+            console.log('browser resize хийж байна');
+            this.browserHeight = window.innerHeight;
+        }, 300));
     }
 
     calcCaller(){
@@ -20,13 +27,15 @@ export default class RevealOnScroll {
     }
 
     calculateIfScrollTo(e){
-        let scrollPercent = (e.getBoundingClientRect().y / window.innerHeight) * 100;
-        if (scrollPercent < 75){
-            e.classList.add('reveal-item--is-visible');
-            e.isRevealed = true;
-            if(e.isLastItem){
-                window.removeEventListener('scroll', this.scrollThroltte);
-            }
+        if(window.scrollY + this.browserHeight > e.offsetTop){
+            let scrollPercent = (e.getBoundingClientRect().y / this.browserHeight) * 100;
+            if (scrollPercent < this.revealPoint){
+                e.classList.add('reveal-item--is-visible');
+                e.isRevealed = true;
+                if(e.isLastItem){
+                    window.removeEventListener('scroll', this.scrollThroltte);
+                }
+        }
         }
     }
 
