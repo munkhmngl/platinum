58b828557157107e1029d50e6b00919cfd52c792
munkhmngl <munkhmngl@gmail.com>
2024-03-05 13:43:44 +0800

    scroll events remove complate

--- app/assets/script/app.js
+++ app/assets/script/app.js
@@ -1,8 +1,10 @@
 import '../styles/style.css';
 import Menu from './modules/mobileMenu';
+import Scroll from './modules/RevealOnScroll';
 
 if(module.hot){
     module.hot.accept();
 };
 
 let mobi = new Menu();
+let content = new Scroll();
